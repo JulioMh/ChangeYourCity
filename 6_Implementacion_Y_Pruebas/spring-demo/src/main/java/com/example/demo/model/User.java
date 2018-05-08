11e051e39fa33a10abee12b44f49044a8c2b5aa7
xfja <franjimenezaguilera@gmail.com>
2018-05-08 11:56:33 +0200

    Modificacion user.java, añadidos atributos extra. Modificacion de usuarios logeados por defecto

--- 6_Implementacion_Y_Pruebas/spring-demo/src/main/java/com/example/demo/model/User.java
+++ 6_Implementacion_Y_Pruebas/spring-demo/src/main/java/com/example/demo/model/User.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2002-2013 the original author o
  */
 package com.example.demo.model;
 
@@ -21,71 +9,91 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-// los usuarios registrados en el sistema se guardan en este tipo de entidad
+//signed up users are saved in this entity
 @Entity
-public class User{
+public class User {
+	@Id
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    private String firstName;
+	private String name;
 
-    private String lastName;
+	private String lastName1;
+	private String lastName2;
 
-    @Column(unique=true, nullable = false)
-    private String email;
+	@Column(unique = true, nullable = false)
+	private String email;
 
-    private String password;
+	private String password;
+	private String municipality;
 
-    public User() {}
+	public User() {
+	}
 
-    public User(User user) {
-        this.id = user.id;
-        this.firstName = user.firstName;
-        this.lastName = user.lastName;
-        this.email = user.email;
-        this.password = user.password;
-    }
+	public User(User user) {
+		this.id = user.id;
+		this.name = user.name;
+		this.lastName1 = user.lastName1;
+		this.lastName2 = user.lastName2;
+		this.email = user.email;
+		this.password = user.password;
+		this.municipality = user.municipality;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName1() {
+		return lastName1;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName1(String lastName1) {
+		this.lastName1 = lastName1;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getLastName2() {
+		return lastName2;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setLastName2(String lastName2) {
+		this.lastName2 = lastName2;
+	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMunicipality() {
+		return municipality;
+	}
+
+	public void setMunicipality(String municipality) {
+		this.municipality = municipality;
+	}
 }
