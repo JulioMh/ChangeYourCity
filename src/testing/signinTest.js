4398c939f7bb2b6e9ccb0aee883ea3c47efd420c
CHERR4 <jramoncaserofuentes@gmail.com>
2018-05-21 16:39:17 +0200

    Otro test añadido por Jose Ramón Casero

--- src/testing/signinTest.js
+++ src/testing/signinTest.js
@@ -2,13 +2,25 @@ const request = require('supertest');
 const app = require('../app');
 const expect = require('chai').expect;
 
-describe('Login API', function() {
+describe('redireccion Login correcta', function() {
     it('Should success if credential is valid', function(done) {
         request(app)
            .post('/auth/signin')
            .send({ username: 'root@root.com', password: 'root' })
            .expect(302)
            .expect('Location', '/')
+           .end(done);
+    });
+});
+
+
+describe('redireccion Login erróneo correcta', function() {
+    it('Should success if credential is not valid', function(done) {
+        request(app)
+           .post('/auth/signin')
+           .send({ username: 's@root.com', password: 'root' })
+           .expect(302)
+           .expect('Location', '/auth/signin')
            .end(done);
     });
 });
