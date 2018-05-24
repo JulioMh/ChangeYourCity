ec987f22df8b79080d8ff80056e367cf90481b59
xfja <franjimenezaguilera@gmail.com>
2018-05-24 12:02:54 +0200

    Esqueleto test de mocks

--- src/testing/mockTest.js
+++ src/testing/mockTest.js
@@ -1,2 +1,8 @@
 const {mock, assert} = require("sinon");
-const database = require("./controllers/UserController")
+const database = require("../controllers/UserController");
+
+it("Mocking queries", () => {
+  var db = mock(database.db); //Mockea mi base de datos
+
+
+});
